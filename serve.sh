a91529b9b2f93fe15a576e63b896ac01a7aee075
Hayabusa58 <hayabusa@hbk1.net>
2025-10-30 22:31:11 +0900

    Update favicon.ico

--- serve.sh
+++ serve.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -u "$(id -u):$(id -g)" -v $PWD:/app --workdir /app -p 8080:8080 ghcr.io/getzola/zola:v0.19.1 serve --interface 0.0.0.0 --port 8080 --base-url localhost
+docker run -u "$(id -u):$(id -g)" -v $PWD:/app --workdir /app -p 8088:8088 ghcr.io/getzola/zola:v0.19.1 serve --interface 0.0.0.0 --port 8088 --base-url localhost
